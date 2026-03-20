f029ccbb64b836670d841d573f873ff8809d0754
YellowZDSh <yellow762001@gmail.com>
2026-03-20 20:52:46 +0700

    20/03/2026
    Change the piano keys, add newer assets & add the player HP display

--- Assets/Scripts/PlayerHP.cs
+++ Assets/Scripts/PlayerHP.cs
@@ -1,15 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHP : MonoBehaviour
 {
     public int maxHP = 5;
-    private int currentHP;
+    public int currentHP;
+    public Texture heartDisplay;
+    public Texture emptyHeart;
+    public RawImage[] heart;
 
     void Start()
     {
         currentHP = maxHP;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < heart.Length; i++)
+        {
+            if (i < currentHP)
+            {
+                heart[i].texture = heartDisplay;
+            }
+            else
+            {
+                heart[i].texture = emptyHeart;
+            }
+
+            if (i < maxHP)
+            {
+                heart[i].enabled = true;
+            }
+            else
+            {
+                heart[i].enabled = false;
+            }
+        }
     }
 
     public void TakeDamage(int amount)
